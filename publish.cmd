9f7ddd26b7c78f698fe5b33095d63ea7ac7a0ddb
Kiran913 <113292267+Kiran913@users.noreply.github.com>
2022-09-29 20:56:54 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\kiran-bot-bot-aa29-Web-Deploy.pubxml -p:Password=rA887166J0AfTwidncarKtT26ijmpFExC9ryHSkGfuSDslnpfGSJrG0t0mcZ
+
